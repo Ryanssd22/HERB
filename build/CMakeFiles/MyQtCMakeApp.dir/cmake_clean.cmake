99d391d7faadb92ec129d5dd1fc9a083328cbb45
igor <igorleeck@gmail.com>
2025-09-06 11:17:05 -0500

    hello world

---
+++ build/CMakeFiles/MyQtCMakeApp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyQtCMakeApp.dir/main.cpp.o"
+  "CMakeFiles/MyQtCMakeApp.dir/main.cpp.o.d"
+  "MyQtCMakeApp"
+  "MyQtCMakeApp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyQtCMakeApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
